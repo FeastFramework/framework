e2eb59b26838a65c48e284c51a807ce746297a4c
Jeremy Presutti <Jeremy@Presutti.us>
2021-10-23 20:13:55 -0400

    Fix false positive bug in latest psalm

--- Router/Router.php
+++ Router/Router.php
@@ -756,12 +756,13 @@ class Router implements ServiceContainerItemInterface, RouterInterface
             $request->clearArguments();
         }
         $i = 0;
+        
         /** @var array<string,string> $arguments */
-
         foreach ($arguments as $key => $val) {
             $request->setArgument($key, $params[$i] ?? $val);
             $i++;
         }
+        /** @psalm-suppress TypeDoesNotContainType  */
         if ($allowVariadic === false || !isset($key) || $i <= 1 || count($params) === $i) {
             return;
         }
