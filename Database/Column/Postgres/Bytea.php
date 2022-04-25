5ff54976ce8d5cae374f91937ecd047a076d5dfd
Jeremy Presutti <Jeremy@Presutti.us>
2022-04-24 21:55:00 -0400

    Copyright fixes

--- Database/Column/Postgres/Bytea.php
+++ Database/Column/Postgres/Bytea.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright 2021 Jeremy Presutti <Jeremy@Presutti.us>
+ * Copyright 2022 Jeremy Presutti <Jeremy@Presutti.us>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
